b04544d8b145f9b64462f813819bdb7ae5358fe8
planeflake <135433211+planeflake@users.noreply.github.com>
2024-04-25 23:38:12 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -5,6 +6,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <nav>
+          <ul className="nav-list">
+            <li><i className="fas fa-dice-d20"></i> Campaigns</li>
+            <li><i className="fas fa-users"></i> Characters</li>
+            <li><i className="fas fa-dragon"></i> Monsters</li>
+            <li><i className="fas fa-magic"></i> Spells</li>
+            <li><i className="fas fa-skull"></i> Effects</li>
+            <li><i className="fas fa-cogs"></i> Settings</li>
+          </ul>
+        </nav>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders navigation menu', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Campaigns/i);
+  expect(linkElement).toBeInTheDocument();
+});
